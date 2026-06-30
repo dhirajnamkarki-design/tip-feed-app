eb8a33d7f1fc5d0273312a5a221476ba4bf63c3b
dhirajnamkarki-design <dhirajnamkarki@gmail.com>
2026-06-30 17:47:23 +1000

    Add files via upload

--- app.py
+++ app.py
@@ -77,4 +77,5 @@ def init():
 
 if __name__ == "__main__":
     aggregator.init_db().close()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
